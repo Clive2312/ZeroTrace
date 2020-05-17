610f887e88feeb4e16a672186c737e4bb817705a
Clive2312 <Clive2312@users.noreply.github.com>
2020-05-17 22:51:10 +0800

    Update ZT_Api.cpp

--- Sample_App/ZT_Api.cpp
+++ Sample_App/ZT_Api.cpp
@@ -17,8 +17,6 @@
 * 
 */
 
-myZT 
-
 class Controller{
   public:
 
@@ -37,7 +35,7 @@ class Controller{
     // return block_id
     uint32_t DumpToZT(unsigned char * addr, uint32_t data_length);
     // return length
-    unsigned char * LoadFromZT(unsigned char * addr, uint32_t block_id);
+    unsigned char * LoadFromZT(unsigned char * addr, uint32_t &block_id);
 
 };
 
